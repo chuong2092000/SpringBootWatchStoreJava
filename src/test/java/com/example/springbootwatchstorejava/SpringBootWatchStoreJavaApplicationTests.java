e50d2ffbbd77635d3a45fd389aef69357d11ed2b
chuong2092000 <122428010+chuong2092000@users.noreply.github.com>
2024-06-24 13:23:07 +0700

    first commit

---
+++ src/test/java/com/example/springbootwatchstorejava/SpringBootWatchStoreJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwatchstorejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWatchStoreJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
